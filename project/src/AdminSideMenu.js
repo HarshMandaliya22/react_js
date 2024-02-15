ebe0642c736fe31a066574510eb2caf6517ee0e9
harshgmandaliya22 <harshmandaliya22@gmail.com>
2024-02-15 20:32:59 +0530

    admin pages routes

--- project/src/AdminSideMenu.js
+++ project/src/AdminSideMenu.js
@@ -1,50 +1,52 @@
-export default function AdminSideMenu()
-{
-    return(<ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-    <li>
-        {/* Sidebar - Brand */}
-        <a className="sidebar-brand" href="admin-home.html">
-            <h3 className><i className="fa-brands fa-shopify" /> Online Shop</h3>
-        </a>
-    </li>
-    {/* Divider */}
-    <hr className="sidebar-divider" />
-    {/* Divider */}
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="admin-home.html">
-            <i className="fa-solid fa-house" />
-            <span>Home</span>
-        </a>
-    </li>
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="admin-category.html">
-            <i className="fa-solid fa-tag" />
-            <span>Category</span>
-        </a>
-    </li>
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="admin-products.html">
-            <i className="fa-solid fa-gift" />
-            <span>Products</span>
-        </a>
-    </li>
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="admin-orders.html">
-            <i className="fa-solid fa-box" />
-            <span>Orders</span>
-        </a>
-    </li>
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="admin-users.html">
-            <i className="fa-solid fa-users" />
-            <span>Users</span>
-        </a>
-    </li>
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="admin-change-password.html">
-            <i className="fa-solid fa-gear" />
-            <span>Change Password</span>
-        </a>
-    </li>
-</ul>);
+import { Link } from 'react-router-dom';
+export default function AdminSideMenu() {
+    return (
+        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <li>
+                {/* Sidebar - Brand */}
+                <Link className="sidebar-brand" to="/home">
+                    <h3 className><i className="fa-brands fa-shopify" /> Online Shop</h3>
+                </Link>
+            </li>
+            {/* Divider */}
+            <hr className="sidebar-divider" />
+            {/* Divider */}
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/home">
+                    <i className="fa-solid fa-house" />
+                    <span>Home</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/category">
+                    <i className="fa-solid fa-tag" />
+                    <span>Category</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/products">
+                    <i className="fa-solid fa-gift" />
+                    <span>Products</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/orders">
+                    <i className="fa-solid fa-box" />
+                    <span>Orders</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/users">
+                    <i className="fa-solid fa-users" />
+                    <span>Users</span>
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/change-password">
+                    <i className="fa-solid fa-gear" />
+                    <span>Change Password</span>
+                </Link>
+            </li>
+        </ul>
+    );
 }
--- project/src/index.js
+++ project/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css';
 import AdminLogIn from './AdminLogIn';
 import AdminForgotPassword from './AdminForgotPassword';
@@ -16,5 +17,30 @@ import AdminProducts from './AdminProducts';
 import AdminProfile from './AdminProfile';
 import AdminUsers from './AdminUsers';
 import AdminViewProduct from './AdminViewProduct';
+let NoPageFound = () => <h1 >No Such Page Exist</h1>
+function ProjectRoute() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AdminLogIn />} />
+                <Route path='/forgot-password' element={<AdminForgotPassword />} />
+                <Route path='/add-category' element={<AdminAddCategory />} />
+                <Route path='/add-product' element={<AdminAddProduct />} />
+                <Route path='/category' element={<AdminCategory />} />
+                <Route path='/change-password' element={<AdminChangePassword />} />
+                <Route path='/edit-category' element={<AdminEditCategory />} />
+                <Route path='/edit-product' element={<AdminEditproduct />} />
+                <Route path='/home' element={<AdminHome />} />
+                <Route path='/orders' element={<AdminOrders />} />
+                <Route path='/order-detail' element={<AdminOrderDetail />} />
+                <Route path='/products' element={<AdminProducts />} />
+                <Route path='/profile' element={<AdminProfile />} />
+                <Route path='/users' element={<AdminUsers />} />
+                <Route path='/view-product' element={<AdminViewProduct />} />
+                <Route path='*' element={<NoPageFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AdminViewProduct/>)
+root.render(<ProjectRoute />)

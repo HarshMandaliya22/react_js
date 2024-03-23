135fd8c25fb37e4988257787064012c8aa2d10e3
harshgmandaliya22 <harshmandaliya22@gmail.com>
2024-03-23 19:54:14 +0530

    index.js

--- exe/src/index.js
+++ exe/src/index.js
@@ -12,6 +12,7 @@ import AdminHome from './AdminHome';
 import AdminLogin from './AdminLogin';
 import AdminService from './AdminService';
 import AdminForgotPassword from './AdminForgotPassword';
+import AdminChangePassword from './AdminChangePassword';
 let NoPageFound = () => <h1 >No Such Page Exist</h1>
 function ProjectRoute() {
     return (
@@ -27,6 +28,7 @@ function ProjectRoute() {
                 <Route path="/city" element={<AdminCity />} />
                 <Route path="/service" element={<AdminService />} />
                 <Route path="/forgot-password" element={<AdminForgotPassword />} />
+                <Route path="/change-password" element={<AdminChangePassword />} />
                 <Route path='*' element={<NoPageFound />} />
             </Routes>
         </BrowserRouter>
